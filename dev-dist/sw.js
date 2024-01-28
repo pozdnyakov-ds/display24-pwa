622483397cb36a43318fdf2ce4a5d8fea223f1c4
Dmitry Pozdnyakov <2903015@gmail.com>
2024-01-28 16:58:02 +0300

    v1.4

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4neobb0ukdo"
+    "revision": "0.069qhqnt9gg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
